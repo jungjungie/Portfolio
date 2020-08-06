34d75d23d6e0e289282b1d91ab1e9c6b0100409e
Esther Min <estherjmin@gmail.com>
2020-08-05 20:11:45 -0400

    installed reactstrap and setup header component with navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Header = (props) => {
+    const [collapsed, setCollapsed] = useState(true);
+
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
+    return (
+        <header>
+            <Navbar color="light" light>
+                <NavbarBrand href="/" className="mr-auto">ESTHER MIN</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink href="/about/">About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/portfolio/">Portfolio</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://drive.google.com/file/d/1wJNz1F6k_fdF96dHhPBqXjC9Bm1QSn14/view" target="_blank">Resume</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/contact/">Contact</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header;

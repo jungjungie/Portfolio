bb13d17cc5af58fa0a3966a4e97f7e308a5352d9
Esther Min <estherjmin@gmail.com>
2020-08-06 18:57:33 -0400

    setup main component for landing page view

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import './Main.css'
 
 function Main() {
     return (
-        <>
-        </>
+        <section>
+            <Container id="main-landing" className="text-center d-flex flex-column justify-content-center">
+                <h1>ESTHER MIN</h1>
+                <h2>Full Stack Web Developer</h2>
+            </Container>
+        </section>
     )
 }
 
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import Project from '../Project/Project';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import projects from '../../projects.json';
 
 function Portfolio() {
     return (
         <section>
-            <Container className="d-flex flex-column align-items-center">
-                {projects.map(project =>
-                    <Project
-                        key={project.id}
-                        title={project.title}
-                        description={project.description}
-                        tech={project.tech}
-                        date={project.date}
-                        gh-url={project["gh-url"]}
-                        deployed-url={project["deployed-url"]}
-                        image={project.image}
-                    />)}
+            <Container>
+                <Row>
+                    <Col>Portfolio</Col>
+                </Row>
+                <Row className="d-flex flex-column align-items-center">
+                    {projects.map(project =>
+                        <Project
+                            key={project.id}
+                            title={project.title}
+                            description={project.description}
+                            tech={project.tech}
+                            date={project.date}
+                            gh-url={project["gh-url"]}
+                            deployed-url={project["deployed-url"]}
+                            image={project.image}
+                        />)}
+                </Row>
             </Container>
         </section>
     )

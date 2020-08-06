379d3c50637e8033efc39238a6c9b4ed51938ac4
Esther Min <estherjmin@gmail.com>
2020-08-06 09:20:20 -0400

    added about component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <About />
       <Footer />
     </>
   );
---
+++ src/components/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+function About() {
+    return (
+        <section>
+            <Container>
+                <Row>
+                    <Col>About Me</Col>
+                </Row>
+                <Row>
+                    <Col xs="4">
+                        <img src="../../../public/images/profilesnap.png" alt="profile" className="image-fluid mr-md-4 mb-4 rounded shadow-sm float-md-left"/>
+                    </Col>
+                    <Col>
+                        <p>Hello! My name is Esther Min.</p>
+
+                        <p>I'm a full-stack web developer. I am currently enrolled full-time in the UPenn LPS Coding Bootcamp (August 2020 completion).</p>
+
+                        <p>My background in hospitality and human resources has given me experience with consulting, data analytics and project work, but my real passion comes from tapping into my creative side. I enjoy problem-solving and bringing code to life for a great user experience!</p>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
+
+export default About;

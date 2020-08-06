a0ac24154802c34f5a1b7ebe45e0aa1cf3bd2b23
Esther Min <estherjmin@gmail.com>
2020-08-05 22:21:26 -0400

    added footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { Row, Col } from 'reactstrap';
+
+const Footer = () => {
+    return (
+        <footer>
+            <Row>
+                <Col className="text-center"><i class="fa fa-envelope " aria-hidden="true"></i></Col>
+                <Col className="text-center"><i class="fa fa-phone" aria-hidden="true"></i></Col>
+                <Col className="text-center"><i class="fa fa-github " aria-hidden="true"></i></Col>
+                <Col className="text-center"><i class="fa fa-linkedin-square " aria-hidden="true"></i></Col>
+            </Row>
+            <Row>
+                <Col className="text-center">Email</Col>
+                <Col className="text-center">267-679-0661</Col>
+                <Col className="text-center">GitHub</Col>
+                <Col className="text-center">LinkedIn</Col>
+            </Row>
+        </footer>
+    );
+}
+
+export default Footer;

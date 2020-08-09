e890b715619a89e02a3bf3642d40efefdd4c0d6a
Esther Min <estherjmin@gmail.com>
2020-08-08 20:26:17 -0400

    Updates

---
+++ precache-manifest.0c9210fecbab1dbe9784458fde9a73bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b844a99846a0843a4c1a5812ef8e0916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5dcf2c7698b0e35035b",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "adc126110adeb0a6bbe6",
+    "url": "/static/css/main.94f555b1.chunk.css"
+  },
+  {
+    "revision": "b5dcf2c7698b0e35035b",
+    "url": "/static/js/2.d0d49fb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d0d49fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc126110adeb0a6bbe6",
+    "url": "/static/js/main.ffb25100.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

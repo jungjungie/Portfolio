35f9f3698350ed0ca6afe4570ed492a019aa8c8b
Esther Min <estherjmin@gmail.com>
2020-08-08 21:07:19 -0400

    fixed responsiveness of project cards and contact cards

---
+++ build/precache-manifest.7b7738288e0afd844e4cfe36849980c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ac648b147b92681420df4fb762969c",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b5dcf2c7698b0e35035b",
+    "url": "/Portfolio/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "adc126110adeb0a6bbe6",
+    "url": "/Portfolio/static/css/main.94f555b1.chunk.css"
+  },
+  {
+    "revision": "b5dcf2c7698b0e35035b",
+    "url": "/Portfolio/static/js/2.d0d49fb1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.d0d49fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adc126110adeb0a6bbe6",
+    "url": "/Portfolio/static/js/main.ffb25100.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c9210fecbab1dbe9784458fde9a73bb.js"
+  "/Portfolio/precache-manifest.7b7738288e0afd844e4cfe36849980c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
